d2ac78c5263e3da632cbb5b54b5453796cc6e5a6
Naqeebali Shamsi <nq957544@dal.ca>
2023-04-24 13:50:46 -0300

    Fix env vars issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
-import dotenv from 'dotenv';
-dotenv.config();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
